714d2352a555b91992398088c864f4853fe4f3e2
Cristhian Soto <cristhian.soto@Tecsup.edu.pe>
2025-09-21 20:59:06 -0500

    feat: set text for main branch

--- app/src/main/java/com/example/lab04/CardExample.kt
+++ app/src/main/java/com/example/lab04/CardExample.kt
@@ -15,7 +15,7 @@ fun CardExample() {
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
         Text(
-            text = "Versión desde conflict-test",
+            text = "Versión desde main",
             modifier = Modifier.padding(16.dp)
         )
     }

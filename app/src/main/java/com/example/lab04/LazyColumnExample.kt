062c8c07895f760b530745ca9871d25c07ddc51e
Cristhian Soto <cristhian.soto@Tecsup.edu.pe>
2025-09-21 20:06:34 -0500

    feat: add LazyColumn component

---
+++ app/src/main/java/com/example/lab04/LazyColumnExample.kt
@@ -0,0 +1,19 @@
+package com.example.lab04
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LazyColumnExample() {
+    val itemsList = listOf("Película 1", "Película 2", "Película 3", "Película 4", "Película 5")
+    LazyColumn(
+        modifier = Modifier.fillMaxSize().padding(16.dp)
+    ) {
+        items(itemsList) { item ->
+            Text(text = item)
+        }
+    }
+}

e2e197fe342632e10c5ac5911ddbadb80874e2b8
Cristhian Soto <cristhian.soto@Tecsup.edu.pe>
2025-09-21 20:11:23 -0500

    feat: add Card component

---
+++ app/src/main/java/com/example/lab04/CardExample.kt
@@ -0,0 +1,22 @@
+package com.example.lab04
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CardExample() {
+    Card(
+        modifier = Modifier.padding(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Text(
+            text = "Soy una Card de ejemplo",
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}

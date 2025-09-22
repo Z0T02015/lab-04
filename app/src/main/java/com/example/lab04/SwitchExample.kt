099a852b70feb001d1c873df4efaed007fae861d
Cristhian Soto <cristhian.soto@Tecsup.edu.pe>
2025-09-21 20:13:35 -0500

    feat: add Switch component

---
+++ app/src/main/java/com/example/lab04/SwitchExample.kt
@@ -0,0 +1,13 @@
+package com.example.lab04
+
+import androidx.compose.material3.Switch
+import androidx.compose.runtime.*
+
+@Composable
+fun SwitchExample() {
+    var checked by remember { mutableStateOf(false) }
+    Switch(
+        checked = checked,
+        onCheckedChange = { checked = it }
+    )
+}
